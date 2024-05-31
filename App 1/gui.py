0efccea29c96271b211f8b7a29c874d225afacea
Jim McKeown <mckeown.james@gmailcom>
2024-05-31 11:27:59 -0400

    "added comments"

--- App 1/gui.py
+++ App 1/gui.py
@@ -6,6 +6,7 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
+#creating window
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
