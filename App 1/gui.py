a305e3f53f0d8963123802324681014a94df6dda
Jim McKeown <mckeown.james@gmailcom>
2024-05-31 11:27:00 -0400

    "updates"

--- App 1/gui.py
+++ App 1/gui.py
@@ -1,6 +1,7 @@
 import functions
 import FreeSimpleGUI as sg
 
+#instsalling gui
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
@@ -8,7 +9,6 @@ add_button = sg.Button("Add")
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
-
 
 
 
