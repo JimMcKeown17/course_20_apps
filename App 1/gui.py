665ef0d864e5b2fcc51cace8adeb93e1dbfd2203
Jim McKeown <mckeown.james@gmailcom>
2024-06-02 21:31:10 -0400

    "more final touches of app"

--- App 1/gui.py
+++ App 1/gui.py
@@ -6,14 +6,21 @@ sg.theme("Black")
 clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2, image_source="add.png",
+                       mouseover_colors="LightBlue2",
+                       tooltip= "Add a todo",
+                       key="Add")
 list_box = sg.Listbox(values=functions.get_todos(),
                       key="todos",
                       enable_events=True,
                       size=[45,10])
 
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(image_source="complete.png",
+                            tooltip="Complete a todo",
+                            key="Complete",
+                            size=2
+                            )
 exit_button = sg.Button("Exit")
 
 
