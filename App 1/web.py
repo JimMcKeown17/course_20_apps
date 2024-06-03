903e6e2a361546957b3fcdbda2d7b4b7cc00eefc
Jim McKeown <mckeown.james@gmailcom>
2024-06-03 18:02:23 -0400

    "streamlit up and running; though statically."

--- App 1/web.py
+++ App 1/web.py
@@ -1,2 +1,15 @@
 import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add a todo...")
 
