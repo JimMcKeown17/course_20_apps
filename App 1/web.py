99a560fac06322701819e30f86f286a63bc7e190
Jim McKeown <mckeown.james@gmailcom>
2024-06-04 08:43:05 -0400

    update some features

--- App 1/web.py
+++ App 1/web.py
@@ -14,10 +14,13 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Add New Todos", placeholder="Add a todo...",
               on_change=add_todo, key='new_todo')
-
-st.session_state
