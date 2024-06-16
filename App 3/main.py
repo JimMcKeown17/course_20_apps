21290d5c842126980dba42390b20274d81863ca1
Jim McKeown <mckeown.james@gmailcom>
2024-06-16 16:01:40 -0400

    rename app4 to app3

--- App 3/main.py
+++ App 3/main.py
@@ -2,18 +2,35 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit="mm", format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    #Set the header
     topic = row["Topic"]
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=topic, align="L", ln=1)
     pdf.line(x1=5,y1=24,x2=200,y2=24)
 
+    #Set footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
     for i in range(row['Pages'] - 1):
+        # Set the header
         pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
